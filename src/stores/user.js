1b58f37a784abfd8df7fbabbb4649a544528f502
脆皮丶 <i@tsuiqg.me>
2024-09-24 16:48:23 +0800

    chore: 修改文件

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,14 +3,14 @@ import { acceptHMRUpdate, defineStore } from 'pinia'
 export const useUserStore = defineStore('userStore', {
   state: () => ({ users: [] }),
   getters: {
-    currentUser: state => state.users.pop(),
+    currentUser: state => state.users.pop()
   },
   actions: {
     setNewUser(name) {
       this.$patch(state => state.users.push(name))
-    },
+    }
   },
-  persist: true,
+  persist: true
 })
 
 if (import.meta.hot)
