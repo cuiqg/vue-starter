d9b30eaa148d9d79124de51e2179dad1deeb60f0
脆皮丶 <cuiqg@foxmail.com>
2025-11-03 01:08:59 +0800

    feat(router): 添加滚动行为和热更新支持

--- src/router.js
+++ src/router.js
@@ -1,10 +1,26 @@
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes)
+  routes: setupLayouts(routes),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    else {
+      return {
+        el: '#app',
+        top: 0,
+        behavior: 'smooth'
+      }
+    }
+  }
 })
+
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
